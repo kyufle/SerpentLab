321ba6ea816728dd6953fa1eafa020f8c0e33d68
ruthromerocarretero <rromerocarretero.cf@iesesteveterradas.cat>
2024-12-02 11:05:00 +0100

    Introduccio

--- uf2/clases/funcion_paquetes.py
+++
@@ -1,7 +0,0 @@
-# import mi_primera_lbreria as funciones_importantes
-# from mi_primera_lbreria import mi_burbuja
-# import mi_modulo.mis_funciones_listas.funciones_ordenacion.mi_primera_lbreria
-from mi_modulo.mis_funciones_listas.funciones_ordenacion import *
-lista = [45,23,4,65,945,2,36,98,2345]
-mi_burbuja(lista,order="asc")
-print(lista)
--- uf2/deberes/practica23/prova.py
+++
@@ -1,5 +0,0 @@
-from matematiques.operacions_basiques import *
-a = 7
-b = 4
-sumar = sumar(a,b)
-print("suma: {}+{}={}".format(str(a),str(b),sumar))
